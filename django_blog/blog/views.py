d9690a1985e70a8be8904e5a0265eb1977b1d9c9
Peter wangimwa <peterwangimwa@gmail.com>
2025-10-04 11:24:11 +0300

    chore: add blog template files for posts, comments, authentication, and search

---
+++ django_blog/blog/views.py
@@ -0,0 +1,110 @@
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from .models import Post, Comment
+from .forms import PostForm, CommentForm, UserRegisterForm, UserUpdateForm
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+
+# ------------------- POST VIEWS -------------------
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    ordering = ["-published_date"]
+    paginate_by = 5
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("post-list")
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+# ------------------- COMMENT VIEW -------------------
+
+@login_required
+def add_comment(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect("post-detail", pk=pk)
+    else:
+        form = CommentForm()
+    return render(request, "blog/comment_form.html", {"form": form})
+
+# ------------------- USER AUTH VIEWS -------------------
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Account created successfully")
+            return redirect("post-list")
+    else:
+        form = UserRegisterForm()
+    return render(request, "blog/register.html", {"form": form})
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, "Profile updated successfully")
+            return redirect("profile")
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+    return render(request, "blog/profile.html", {"u_form": u_form})
+
+# ------------------- SEARCH -------------------
+
+class SearchResultsView(ListView):
+    model = Post
+    template_name = "blog/search_results.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        if query:
+            return Post.objects.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(tags__name__icontains=query)
+            ).distinct()
+        return Post.objects.none()
